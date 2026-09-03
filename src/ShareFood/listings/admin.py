2e055d0590338000a55859752e4b40924b43fbf2
Kateryna <katy96ms@gmail.com>
2026-09-03 21:23:34 +0300

    Configure gitignore for aider and register FoodListing in admin

--- src/ShareFood/listings/admin.py
+++ src/ShareFood/listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FoodListing
 
-# Register your models here.
+
+@admin.register(FoodListing)
+class FoodListingAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "district", "expiration_date")
+    list_filter = ("category", "district")
